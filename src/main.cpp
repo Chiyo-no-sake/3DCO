14caa995ea8d8250374e1487bd88c7bf26e592a7
Luca Pasini <l.pasini078@gmail.com>
2021-03-10 09:11:26 +0100

    added fix cmake

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,22 @@
 
 
 int main(int argc, char* argv[]){
-    Log::getInstance()->setLevel(spdlog::level::info);
+    Log::getInstance()->setLevel(spdlog::level::trace);
 
     FileParser fp{};
     coScene* scene;
     fp.setStrategy(new AssimpStrategy());
     scene = fp.loadFromFile(std::string(ASSETSDIR) + std::string(FILENAME));
+
+    // auto p = new Pipeline();
+    // p.addStep(new IPipeElement());
+    // ....
+    // p.execute();
+    //      | class
+    //      | --> for(auto& element: pipeElements){
+    //      |             scene = element.execute(scene);
+    //      |     }
+    //      |     return scene;
+    // outwriter(p.getResult());
+
 }
